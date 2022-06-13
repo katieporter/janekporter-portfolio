49b21c590619d9cc9b731ad032aa9f3845f12427
Katie Porter <katie@janekporter.com>
2022-06-13 08:10:09 -0600

    Redirect all traffic to https

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ const htmlSerializer = (node, children) => {
     return null;
 }
 
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] !== 'https') return res.redirect('https://' + req.headers.host + req.url);
+        else return next();
+    } else return next();
+});
+
 app.listen(PORT, () => {
     Onboarding.trigger();
     process.stdout.write(`Point your browser to: http://localhost:${PORT}\n`);
